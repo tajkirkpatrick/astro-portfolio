d593517a9dda606717e5ea90365105d6b6c1dc9e
Taj K <taj.kirkpatrick@gmail.com>
2023-11-28 21:42:12 -0600

    Add sst-env.d.ts, update .gitignore, and configure Astro and SST

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from "astro/config";
+import aws from "astro-sst";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
-    ,
     react(),
   ],
+  output: "server",
+  adapter: aws(),
 });
